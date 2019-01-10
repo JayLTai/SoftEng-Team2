ccf9dae455985eb327d0672c6067a9e3fb54d24f
Ben Waid <benmw10@gmail.com>
2019-01-10 14:46:27 -0500

    Added me

--- teamlist.java
+++ teamlist.java
@@ -2,4 +2,5 @@ public static void main(String [ ] args)
         {
         System.out.println("My name is Haozhe Jiang");
         System.out.println("Justine Roy!");
+        System.out.println("Hi I'm Ben Waid");
         }
